a89f0f28ae5d3a8cfab1f9cc1fda3b14b96e2c3d
henriquegomesteixeira <henriquegomes080012@gmail.com>
2023-12-01 05:25:22 -0300

    [Dev] Henrique Gomes Teixeira

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    students_count = 0
+    try:
+        for min_hours, max_hours in permanence_period:
+            if min_hours <= target_time <= max_hours:
+                students_count += 1
+        return students_count
+    except TypeError:
+        return None
