b159ec9460019ffca873578d67d86859d006a41d
henriquegomesteixeira <henriquegomes080012@gmail.com>
2023-12-02 00:00:19 -0300

    Adicionado teste de encrypt_message e tratamento de exceção para argumentos inválidos

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("message", 2) == "egass_em"
+    assert encrypt_message("message", 3) == "sem_egas"
+    assert encrypt_message("message", 7) == "egassem"
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 5)
